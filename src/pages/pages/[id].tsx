714c64f68508f8424bb94ba71bb54b6b7dfd684e
Marco <mstn@posteo.org>
2020-04-02 16:35:23 +0200

    wip

--- src/pages/pages/[id].tsx
+++ src/pages/pages/[id].tsx
@@ -1,96 +1,68 @@
 import React from 'react';
 
-import { gql } from 'apollo-boost';
-
-import { useApolloClient } from '@apollo/react-hooks';
-
 import { useRouter } from 'next/router';
-
-import initApolloClient from '../../lib/apolloClient';
 
 import CompanySearchPage from '../../components/CompanySearchPage';
 
-const client = initApolloClient();
+const applyFilter = (companies, currentPage, search) => {
+  const filteredCompanies = companies.filter( 
+    company => company.address.startsWith(search)
+  );
 
-const GET_COMPANIES = gql`
-  query getCompanies($search: CompanySearchInput, $offset: Int, $limit: Int) {
-    getCompanies(search: $search, offset: $offset, limit: $limit) {
-      totalCount
-      data {
-        id
-        name
-        address
-      }
-    }
+  return {
+    companies: filteredCompanies.slice((currentPage-1)*10, (currentPage-1)*10 + 10),
+    totalCount: filteredCompanies.length,
   }
-`;
+}
 
-const Page = (staticProps: any) => {
+const Page = (props: any) => {
+  
   const router = useRouter();
-
   const search = router.query.search || '';
-  let isLoading = false;
 
-  if ((global as any).window) {
-    console.log(window.location.search)
-    isLoading = window.location.search.startsWith('?search=')
-    console.log(isLoading)
-  }
-
-  const [state, setState] = React.useState({...staticProps, isLoading });
-  const client = useApolloClient();
+  // if search !== '', show loading wheel
+  const [state, setState] = React.useState(applyFilter(props.companies, props.currentPage, search));
   
   const setSearch = (keyword: string) => router.push('/?search=' + keyword);
 
-  const currentPage = parseInt(router.query.id as string)
-
   React.useEffect(() => {
-    client.query({
-      query: GET_COMPANIES,
-      variables: {
-        search: { keyword: search },
-        offset: (currentPage-1)*10,
-        limit: 10,
-      }
-    }).then(result => {
-      setState({
-        ...state,
-        isLoading: false,
-        totalCount: result.data.getCompanies.totalCount,
-        companies: result.data.getCompanies.data
-      })
-    })
+    setState(applyFilter(props.companies, props.currentPage, search))
   }, [search]);
 
   return (
-    <CompanySearchPage {...state} onSearch={setSearch} keyword={search} currentPage={currentPage} />
+    <CompanySearchPage {...state} onSearch={setSearch} keyword={search} currentPage={props.currentPage} />
   );
 }
 
-export async function getServerSideProps({ params, query }) {
-  const pageId = parseInt(params.id);
-  let totalCount = 0;
-  let companies = [];
+export async function getStaticPaths() {
 
-  try {
-    const result = await client.query({
-      query: GET_COMPANIES,
-      variables: {
-        search: { keyword: query.search },
-        offset: (pageId-1)*10,
-        limit: 10,
-      }
-    });
-    // TODO check errors
+  const companies = require('../../../mocks/companies.json');
+  const totalCount = companies.length;
+  const paths = [];
     
-    totalCount = result.data.getCompanies.totalCount;
-    companies = result.data.getCompanies.data;
-
-  } catch (e) {
-    console.error('Cannot call GraphQL server %j', e);
+  for (let i=0; i<Math.ceil(totalCount/10); i++) {
+    const pageId = i + 1;
+    paths.push(`/pages/${pageId}`);
   }
-  // Pass post data to the page via props
-  return { props: { companies, totalCount, currentPage: pageId } }
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+
+export async function getStaticProps({ params }) {
+  const pageId = params.id;
+  const companies = require('../../../mocks/companies.json');
+
+  return {
+    props: {
+      companies,
+      totalCount: companies.length,
+      currentPage: parseInt(pageId)
+    }, 
+  }
 }
 
 export default Page
