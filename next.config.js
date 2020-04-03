e13788cef852a223822ee706e6de9f1697bb86c6
Marco <mstn@posteo.org>
2020-04-03 13:41:38 +0200

    fix relative paths on gh pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const isGithubPages = !!process.env.GITHUB_PAGES;
+const assetPrefix = isGithubPages ? '/aah' : '';
+
 module.exports = {
+  assetPrefix,
   env: {
     showAddCompanyForm: false,
-    localDataFolder: 'mocks'
+    localDataFolder: 'mocks',
+    ASSET_PREFIX: assetPrefix,
   },
 }
